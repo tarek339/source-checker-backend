ffc23fa87dfb3fd4867ee41458d4960d9b1a1b67
Tarek Jassine <tarekjassine@gmail.com>
2024-07-03 13:39:30 +0200

    root directory

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 require("dotenv").config();
 
 export const uploadImg = (img: Buffer, imgName: string) => {
-  const screenshot = process.env.WEB_SERVER_URL + "/" + imgName;
+  const screenshot = process.env.WEB_SERVER_URL + "-" + imgName;
   const filePath = `/Root/to/Directory/${screenshot}`;
   fs.writeFileSync(filePath, img);
 
