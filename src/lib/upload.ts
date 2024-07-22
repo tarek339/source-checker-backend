b4589ed8b1f74143ca79fb800d85e639d61b9afb
Tarek Jassine <tarekjassine@gmail.com>
2024-07-22 21:18:36 +0200

    debug clg

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -4,6 +4,9 @@ require("dotenv").config();
 export const uploadImg = (img: Buffer, imgName: string) => {
   const screenshot = imgName;
   const filePath = process.env.ROOT_TO_DIRECTORY + screenshot;
+  console.log("root", process.env.ROOT_TO_DIRECTORY);
+  console.log("sh", screenshot);
+  console.log("path", filePath);
   fs.writeFile(filePath, img, (err) => {
     console.log(err);
   });
