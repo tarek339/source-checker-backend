a46d3a06458a2241c76253b690e59a0083c49054
Tarek Jassine <tarekjassine@gmail.com>
2024-07-23 13:41:06 +0200

    debug

--- src/controllers/survey.ts
+++ src/controllers/survey.ts
@@ -100,28 +100,26 @@ export const completeSurvey = async (req: Request, res: Response) => {
       survey,
     });
   } catch (error) {
-    console.log(
-      "Capture screenshot timeoutError: Navigation timeout of 30000 ms exceeded. Creating open graph data only."
-    );
-    try {
-      const openGraphData = await scrapOpenGraph(req.body.page.url);
-      req.body.page.openGraph = openGraphData;
-      survey.pages.push(req.body.page);
-      await survey.save();
-      res.json({
-        message:
-          "Unable to caputure screenshots for this website. Created open graph data only.",
-        survey,
-      });
-    } catch (error) {
-      console.log(
-        "Unable to create screeshots and open graph data. Please try another website!"
-      );
-      res.status(422).json({
-        message:
-          "Unable to create screeshots and open graph data. Please try another website!",
-      });
-    }
+    console.log(error);
+    // try {
+    //   const openGraphData = await scrapOpenGraph(req.body.page.url);
+    //   req.body.page.openGraph = openGraphData;
+    //   survey.pages.push(req.body.page);
+    //   await survey.save();
+    //   res.json({
+    //     message:
+    //       "Unable to caputure screenshots for this website. Created open graph data only.",
+    //     survey,
+    //   });
+    // } catch (error) {
+    //   console.log(
+    //     "Unable to create screeshots and open graph data. Please try another website!"
+    //   );
+    //   res.status(422).json({
+    //     message:
+    //       "Unable to create screeshots and open graph data. Please try another website!",
+    //   });
+    // }
   }
 };
 
