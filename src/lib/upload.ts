7952818ca200b0d06b9351193a7cdea184194c05
Tarek Jassine <tarekjassine@gmail.com>
2024-08-31 13:14:32 +0200

    sycroned fn

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -4,9 +4,6 @@ require("dotenv").config();
 export const uploadImg = (img: Buffer, imgName: string) => {
   const screenshot = imgName;
   const filePath = process.env.ROOT_TO_DIRECTORY + screenshot;
-  fs.writeFile(filePath, img, (err) => {
-    console.log(err);
-  });
-
+  fs.writeFileSync(filePath, img);
   return filePath;
 };
