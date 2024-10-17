e5ec9112b63db58b7552e3dd57e6253410998e4e
Tarek Jassine <tarekjassine@gmail.com>
2024-10-17 23:30:18 +0200

    upgrade fn for insta

--- src/lib/screenshot.ts
+++ src/lib/screenshot.ts
@@ -47,19 +47,7 @@ export const captureScreenshot = async (
         await tab.checked;
         await tab.doOptIn();
       } catch (e) {}
-    }
-    // else if (url.includes("https://www.facebook.com")) {
-    //   console.log("facebook");
-    //   const facebook = ".x92rtbv";
-    //   await page.waitForSelector(facebook, { visible: true });
-    //   await page.click(facebook);
-    //   const tab = autoconsent.attachToPage(page, url, rules, 10);
-    //   try {
-    //     await tab.checked;
-    //     await tab.doOptIn();
-    //   } catch (e) {}
-    // }
-    else {
+    } else {
       const tab = autoconsent.attachToPage(page, url, rules, 10);
       try {
         await tab.checked;
