f5e85e0678d36c8f63d7c1941a61e156cd359bfc
Tarek Jassine <tarekjassine@gmail.com>
2024-11-09 18:52:49 +0100

    optimized strt command

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,9 +1,9 @@
 import fs from "fs";
 require("dotenv").config();
 
-export const uploadImg = (img: Buffer, imgName: string) => {
-  const screenshot = imgName;
-  const filePath = process.env.ROOT_TO_DIRECTORY + screenshot;
-  fs.writeFileSync(filePath, img);
-  return filePath;
-};
+// export const uploadImg = (img: Buffer, imgName: string) => {
+//   const screenshot = imgName;
+//   const filePath = process.env.ROOT_TO_DIRECTORY + screenshot;
+//   fs.writeFileSync(filePath, img);
+//   return filePath;
+// };
